31469d4b92a75c8d3b8910cdbc085b435afe45e0
Mike Gray <mike@graywind.org>
2023-08-04 21:41:25 -0500

    chore: project created with projen

---
+++ tests/test_example.py
@@ -0,0 +1,15 @@
+import pytest
+
+from skill_mark2_audio_receiver.example import hello
+
+@pytest.mark.parametrize(
+    ("name", "expected"),
+    [
+        ("A. Musing", "Hello A. Musing!"),
+        ("traveler", "Hello traveler!"),
+        ("projen developer", "Hello projen developer!"),
+    ],
+)
+def test_hello(name, expected):
+    """Example test with parametrization."""
+    assert hello(name) == expected
